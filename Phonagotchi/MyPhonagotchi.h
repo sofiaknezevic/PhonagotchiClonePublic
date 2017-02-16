bbf5d23efd6bd25c30dec2db178d38587b524310
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-16 11:19:15 -0800

    Initial commit

---
+++ Phonagotchi/MyPhonagotchi.h
@@ -0,0 +1,17 @@
+//
+//  MyPhonagotchi.h
+//  Phonagotchi
+//
+//  Created by Sofia Knezevic on 2017-02-16.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyPhonagotchi : NSObject
+
+@property (readonly) BOOL isPetGrumpy;
+
+- (void) pettingVelocity:(CGPoint) velocity;
+
+@end
---
+++ Phonagotchi/MyPhonagotchi.m
@@ -0,0 +1,18 @@
+//
+//  MyPhonagotchi.m
+//  Phonagotchi
+//
+//  Created by Sofia Knezevic on 2017-02-16.
+//  Copyright © 2017 Lighthouse Labs. All rights reserved.
+//
+
+#import "MyPhonagotchi.h"
+
+@implementation MyPhonagotchi
+
+- (void)pettingVelocity:(CGPoint)velocity
+{
+    
+}
+
+@end
